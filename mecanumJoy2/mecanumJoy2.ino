61d4539256dc5016edfa87b50749a7050794dbad
Dan Royer <dan@marginallyclever.com>
2018-02-07 15:57:42 -0800

    final version for 2018-02-07

--- mecanumJoy2/mecanumJoy2.ino
+++ mecanumJoy2/mecanumJoy2.ino
@@ -20,13 +20,85 @@ int convert(int arg0) {
   Serial.print(arg1);
   Serial.print('\t');
   
-  if(arg1>DEADZONE) return 1;
-  if(arg1<DEADZONE) return -1;
+  if(arg1> DEADZONE) return 1;
+  if(arg1<-DEADZONE) return -1;
   return 0;
 }
 
 
+/**
+ * @input x x-axis value of joystick
+ * @input y y-axis value of joystick
+ * @return 0=deadzone, 1=east, 2=north, 3=west, 4=south
+ */
+int getMajorAxis(int x,int y) {
+  x -= MIDDLE;
+  y -= MIDDLE;
+  
+  //Serial.print(x);  Serial.print('\t');
+  //Serial.print(y);  Serial.print('\t');
+  
+  int ax = abs(x);
+  int ay = abs(y);
+  if(ax<DEADZONE && ay<DEADZONE) return 0;
+  else if(ax>DEADZONE && ay>DEADZONE) {
+    if(ax>ay) {
+      if(x>0) return 1;
+      else return 3;
+    } else {
+      if(y>0) return 2;
+      else return 4;
+    }
+  } else if(ax>DEADZONE) {
+    if(x>0) return 1;
+    else return 3;
+  } else {  // ay>DEADZONE
+    if(y>0) return 2;
+    else return 4;
+  }
+}
+
+int driveStateOld=0;
+
 void loop() {
+  //singleAxisTest();
+  int right = getMajorAxis(analogRead(0),analogRead(1));
+  int left  = getMajorAxis(analogRead(2),analogRead(3));
+
+  // find the desired drive state
+  int driveState = driveStateOld;
+  if(left==2) {
+    if(right==2) driveState=1;
+    if(right==4) driveState=6;
+  }
+  if(left==4) {
+    if(right==2) driveState=5;
+    if(right==4) driveState=3;
+  }
+  if(left==1 && right==1) driveState=4;
+  if(left==3 && right==3) driveState=2;
+  if(left==0 && right==0) driveState=0;
+
+  // only trasmit changes to drive state
+  if(driveStateOld != driveState) {
+    driveStateOld = driveState;
+    switch(driveState) {
+      case  1:  Serial.print('W');  break;
+      case  2:  Serial.print('A');  break;
+      case  3:  Serial.print('S');  break;
+      case  4:  Serial.print('D');  break;
+      case  5:  Serial.print('Q');  break;
+      case  6:  Serial.print('E');  break;
+      default:  Serial.print(' ');  break;
+    }
+    Serial.print("\n");
+  }
+
+  delay(50);
+}
+
+
+void singleAxisTest() {
   int rx = convert(analogRead(0));
   int ry = convert(analogRead(1));
   int lx = convert(analogRead(2));
